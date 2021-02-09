925f3957080b3294f764a0988f124a3e364b5897
wilbert-chen <wilbert.chen@udngroup.com.tw>
2021-02-09 17:42:20 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Table from './Table';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Table />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
